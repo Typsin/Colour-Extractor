593097a26d62a3a16dc05d383de44d088d660b74
Muhammad Nasharuddin <92113878+Typsin@users.noreply.github.com>
2022-08-06 00:21:51 +0800

    Add files via upload

---
+++ mixed_color_extractor.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# load the image
+image = cv2.imread('colour_venn_diagram.jpg')
+cv2.imshow('Orignal', image)
+rgb_ver = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+hsv_ver = cv2.cvtColor(rgb_ver, cv2.COLOR_RGB2HSV)
+
+Red_L = np.array([0, 40, 100])
+Red_U = np.array([50, 50, 255])
+mask = cv2.inRange(image, Red_L, Red_U)
+res = cv2.bitwise_and(image, image, mask=mask)
+cv2.imshow('red', res)
+
+GreenL = np.array([45, 150, 50])
+GreenU = np.array([85, 255, 255])
+mask = cv2.inRange(hsv_ver, GreenL, GreenU)
+res = cv2.bitwise_and(rgb_ver, rgb_ver, mask=mask)
+cv2.imshow('Green', res)
+
+BlueL = np.array([98, 150, 0])
+BlueU = np.array([125, 255, 255])
+mask = cv2.inRange(hsv_ver, BlueL, BlueU)
+res = cv2.bitwise_and(image, image, mask=mask)
+cv2.imshow('Blue', res)
+
+YellowL = np.array([25,150, 50])
+YellowU = np.array([35, 255, 255])
+mask = cv2.inRange(hsv_ver, YellowL, YellowU)
+res = cv2.bitwise_and(image, image, mask=mask)
+cv2.imshow('yellow', res)
+
+CyanL = np.array([85,150, 0])
+CyanU = np.array([95, 255, 255])
+mask = cv2.inRange(hsv_ver, CyanL, CyanU)
+res = cv2.bitwise_and(image, image, mask=mask)
+cv2.imshow('Cyan', res)
+
+MenL = np.array([145,150, 0])
+MenU = np.array([155, 255, 255])
+mask = cv2.inRange(hsv_ver, MenL, MenU)
+res = cv2.bitwise_and(rgb_ver, rgb_ver, mask=mask)
+cv2.imshow('Magenta', res)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
